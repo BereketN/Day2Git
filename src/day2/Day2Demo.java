03207a8a97cf9d75cb237f4af58f1f2deede2a29
bruka <bruka@LAPTOP-43QTSTF8>
2023-04-07 18:06:47 -0700

    Added some notes to the getInfo()

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -5,4 +5,14 @@ public class Day2Demo {
 	public void getInfo() {
 		System.out.println("Pushing code...");
 	}
+	
+	public void add() {
+		System.out.println("2+3+5");
+	}
+	
+	public void deducte() {
+		System.out.println("2-3-5");
+	}
+	
+	
 }
